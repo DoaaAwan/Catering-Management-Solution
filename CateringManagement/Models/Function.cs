6fd76cffa2381501e2c0bde01661b827de95f7a4
Doaa-Awan <146845118+doaaawan@users.noreply.github.com>
2023-10-11 18:02:43 -0400

    Added MealType class
    - created new class
    - changed fk property in function.cs to MealType object

--- CateringManagement/Models/Function.cs
+++ CateringManagement/Models/Function.cs
@@ -108,7 +108,7 @@ namespace CateringManagement.Models
         public int? MealTypeID { get; set; } //MealTypeID (not required)
 
         [Display(Name = "Meal Type")]
-        public FunctionType MealType { get; set; } //MealType
+        public MealType MealType { get; set; } //MealType
 
 
         [Display(Name = "Customer")]
---
+++ CateringManagement/Models/MealType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CateringManagement.Models
+{
+    public class MealType
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Meal Type")]
+        [Required(ErrorMessage = "You cannot leave the meal type blank.")]
+        [StringLength(80, ErrorMessage = "Meal type cannot be more than 80 characters long.")] //max length = 80
+        public string Name { get; set; }
+
+        public ICollection<Function> Functions { get; set; } = new HashSet<Function>();
+    }
+}
