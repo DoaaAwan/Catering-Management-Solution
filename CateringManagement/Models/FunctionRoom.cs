b64d88431baeae8a1fe62bad812ac7604d63e6b3
Doaa-Awan <146845118+doaaawan@users.noreply.github.com>
2023-10-11 19:04:35 -0400

    Updated FunctionRoom and Room classes

--- CateringManagement/Models/FunctionRoom.cs
+++ CateringManagement/Models/FunctionRoom.cs
@@ -1,6 +1,22 @@
-﻿namespace CateringManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CateringManagement.Models
 {
     public class FunctionRoom
     {
+        [Display(Name = "Function")]
+        [Required(ErrorMessage = "You must select a Function")]
+        public int FunctionID { get; set; } //FunctionID (PK)
+
+        [Display(Name = "Function")]
+        public Function Function { get; set; } //Function (FK)
+
+
+        [Display(Name = "Room")]
+        [Required(ErrorMessage = "You must select a Room")]
+        public int RoomID { get; set; } //RoomID (PK)
+
+        [Display(Name = "Room")]
+        public Room Room { get; set; } //Room (FK)
     }
 }
--- CateringManagement/Models/Room.cs
+++ CateringManagement/Models/Room.cs
@@ -7,13 +7,13 @@ namespace CateringManagement.Models
         public int ID { get; set; }
 
         [Display(Name = "Room")]
-        [Required(ErrorMessage = "You cannot leave the room blank.")]
+        [Required(ErrorMessage = "You cannot leave the Room blank.")]
         [StringLength(80, ErrorMessage = "Room cannot be more than 80 characters long.")] //max length = 80
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "You cannot leave the capacity blank.")] //required
-        public int Capacity { get; set; } //must be > 0
+        [Required(ErrorMessage = "You cannot leave the Capacity blank.")] //required
+        public int Capacity { get {  return Capacity; } set { if (value > 0) Capacity = value; } } //must be > 0
 
-        //public ICollection<Function> Functions { get; set; } = new HashSet<Function>();
+        public ICollection<FunctionRoom> FunctionRooms { get; set; } = new HashSet<FunctionRoom>();
     }
 }
