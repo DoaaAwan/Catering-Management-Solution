fb65657c7eb8e517189f624eaab2c159fc221834
Doaa Awan <146845118+doaaawan@users.noreply.github.com>
2023-10-13 21:40:15 -0400

    Add Auditing to automatically maintain the four basic audit fields

--- CateringManagement/Program.cs
+++ CateringManagement/Program.cs
@@ -1,6 +1,7 @@
 using CateringManagement.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddDbContext<CateringContext>(options =>
     options.UseSqlite(connectionString));
+
+//To give access to IHttpContextAccessor for Audit Data with IAuditable
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
